15d777fa21091511535aca47e5102413b17a9639
student6 <student6@pcoip.ki.agh.edu.pl>
2019-05-12 11:10:42 +0200

    first test added

--- src/test/java/com/company/enroller/controllers/ParticipantRestControllerTest.java
+++ src/test/java/com/company/enroller/controllers/ParticipantRestControllerTest.java
@@ -5,6 +5,9 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -22,6 +25,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import com.company.enroller.model.Participant;
 import com.company.enroller.persistence.MeetingService;
 import com.company.enroller.persistence.ParticipantService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(ParticipantRestController.class)
@@ -34,6 +38,7 @@ public class ParticipantRestControllerTest {
 	private MeetingService meetingService;
 
 	@MockBean
+	// @Autowired - test integracyjny z serwisem
 	private ParticipantService participantService;
 
 	@Test
@@ -48,5 +53,71 @@ public class ParticipantRestControllerTest {
 		mvc.perform(get("/participants").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
 				.andExpect(jsonPath("$", hasSize(1))).andExpect(jsonPath("$[0].login", is(participant.getLogin())));
 	}
+
+	@Test
+	public void getParticipant() throws Exception {
+		Participant participant = new Participant();
+		participant.setLogin("testlogin");
+		participant.setPassword("testpassword");
+
+		given(participantService.findByLogin("testlogin")).willReturn(participant);
+
+		// mvc.perform(get("/participants/testlogin").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+		// .andExpect(content().string("TUTAJ WPISZ OCZEKIWANEGO JSONa"));
+
+		// rozne mozliwosci sprawdzenie oczekiwanych danych - ten wydaje sie
+		// najfajniszy automatycznie zmianie obiekt na JSona
+		mvc.perform(get("/participants/testlogin").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(content().string(new ObjectMapper().writeValueAsString(participant)));
+
+	}
+
+	@Test
+	public void addParticipantTest() throws Exception {
+		Participant participant = new Participant();
+		participant.setLogin("testlogin");
+		participant.setPassword("testpassword");
+		
+		String participantJson = new ObjectMapper().writeValueAsString(participant);
+
+		given(participantService.add(participant)).willReturn(null);
+
+		mvc.perform(post("/participants").content(participantJson).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isCreated())
+				.andExpect(content().string(participantJson));
+		
+	}
+	
+	@Test
+	public void addParticipantExistiongTest() throws Exception {
+		Participant participant = new Participant();
+		participant.setLogin("testlogin");
+		participant.setPassword("testpassword");
+		
+		String participantJson = new ObjectMapper().writeValueAsString(participant);
+
+		given(participantService.findByLogin("testlogin")).willReturn(participant);
+
+		mvc.perform(post("/participants").content(participantJson).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isConflict());
+		
+	}
+	
+//	@Test
+//	public void deleteParticipantExistiongTest() throws Exception {
+//		Participant participant = new Participant();
+//		participant.setLogin("testlogin");
+//		participant.setPassword("testpassword");
+//		
+//		String participantJson = new ObjectMapper().writeValueAsString(participant);
+//
+//		given(participantService.findByLogin("testlogin")).willReturn(participant);
+//		veryfi(participantService, never()).finfbyLogin("testk");
+//		veryfi(participantService, atLeastOnce()).finfbyLogin("testk");
+//
+//		mvc.perform(delete("/participants/testlogin").content(participantJson).contentType(MediaType.APPLICATION_JSON))
+//				.andExpect(status().isNoContent());
+//		
+//	}
 
 }
